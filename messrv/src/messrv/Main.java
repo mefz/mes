6d1c92b7ecbe63e42e0b017ec27f6cc4b27245af
mnugumanov <mnugumanov@go-mos-0082.psi.de>
2016-10-06 14:59:13 +0300

    srv multicli

--- messrv/src/messrv/Main.java
+++ messrv/src/messrv/Main.java
@@ -10,27 +10,54 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 
-public class Main {
+public class Main extends Thread {
+	private Socket socket;
+	private BufferedReader in;
+	private PrintWriter out;
+	
+	public Main(Socket s) {
+		socket = s;
+		try {
+			in = new BufferedReader(new InputStreamReader(
+			        socket.getInputStream()));
+			out = new PrintWriter(new BufferedWriter(
+			        new OutputStreamWriter(socket.getOutputStream())), true);
+		} catch (IOException e) {
+			System.out.println("Buffer error: "+e);
+		}
+		start();
+	}
+	public void run(){
+		try {
+			while(true) {
+				String str = in.readLine();
+				if (str.equals("END"))
+					break;
+			    System.out.println("Echoing: " + str);
+			    out.println(str);
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		} 
+		finally {
+			try {
+				socket.close();
+				System.out.println("Closed socket "+socket);
+			} catch (IOException e) {
+				System.out.println("Error closing socket "+e);
+			}
+		}
+		
+		
+	}
 
 	public static void main(String[] args) throws IOException {
 		ServerSocket s = new ServerSocket(2345);
-		System.out.println("New socket: "+s);
-		Socket socket = s.accept();
-		System.out.println("Connection accepted: "+socket);
-		BufferedReader in = new BufferedReader(new InputStreamReader(
-                socket.getInputStream()));
-		PrintWriter out = new PrintWriter(new BufferedWriter(
-                new OutputStreamWriter(socket.getOutputStream())), true);
+		System.out.println("New socket: "+s);			
 		while (true) {
-            try {
-				String str = in.readLine();
-				if (str.equals("END"))
-				   break;
-				System.out.println("Echoing: " + str);
-				out.println(str);
-			} catch (Exception e) {
-				System.out.println("Communication to client error:"+e);
-			}
+			Socket socket = s.accept();
+			System.out.println("Connection accepted: "+socket);
+            new Main(socket);
          }
 	}
 
